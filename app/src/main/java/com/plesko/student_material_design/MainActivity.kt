70a28f55400576f407fd227abe04e059a73ef6a8
Danny227 <dplesko2007@gmail.com>
2026-04-11 01:29:15 +0300

    Step 6: Custom color scheme added

--- app/src/main/java/com/plesko/student_material_design/MainActivity.kt
+++ app/src/main/java/com/plesko/student_material_design/MainActivity.kt
@@ -25,6 +25,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.res.painterResource
@@ -84,20 +85,28 @@ class MainActivity : ComponentActivity() {
         student: Student,
         modifier: Modifier = Modifier
     ) {
-        Row(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(dimensionResource(R.dimen.padding_small))
-        ) {
-            StudentIcon(student.imageResourceId)
-            StudentInformation(student.name, student.age)
+        Card(modifier = modifier) {
+            Row(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(R.dimen.padding_small))
+            ) {
+                StudentIcon(student.imageResourceId)
+                StudentInformation(student.name, student.age)
+            }
         }
     }
     @Composable
     fun StudentApp() {
-        LazyColumn {
-            items(students) {
-                StudentItem(student = it)
+        Scaffold { it ->
+            LazyColumn(contentPadding = it) {
+                items(students) {
+                    StudentItem(
+                        student = it,
+                        modifier = Modifier
+                            .padding(dimensionResource(R.dimen.padding_small))
+                    )
+                }
             }
         }
     }
