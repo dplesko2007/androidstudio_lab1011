3bb397da16bf084ec89d05714c7b595c7f882242
Danny227 <dplesko2007@gmail.com>
2026-04-11 16:55:42 +0300

    Step 9: Custom typography added

--- app/src/main/java/com/plesko/student_material_design/ui/theme/Type.kt
+++ app/src/main/java/com/plesko/student_material_design/ui/theme/Type.kt
@@ -5,15 +5,37 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.plesko.student_material_design.R
+import androidx.compose.ui.text.font.Font
 
 // Set of Material typography styles to start with
+val AbrilFatface = FontFamily(
+    Font(R.font.abril_fatface_regular)
+)
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+)
 val Typography = Typography(
-    bodyLarge = TextStyle(
+    displayLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 36.sp,
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
